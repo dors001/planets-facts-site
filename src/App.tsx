6a0cf8a0c52ee0bbd96b09e3b71b968c921a9562
dors001 <dorarishani@gmail.com>
2023-08-16 00:08:28 +0300

    Define Logo component

--- src/App.tsx
+++ src/App.tsx
@@ -13,6 +13,7 @@ import {
 import "./App.css";
 import "./index.css";
 import theme from "./theme";
+import Logo from "./Components/Logo";
 
 function App() {
   const { isOpen, onToggle } = useDisclosure();
@@ -20,28 +21,15 @@ function App() {
   return (
     <>
       <VStack>
-        <Flex className="header" border="1px solid yellow">
-          <Box className="logo" backgroundColor="blue" textStyle="h3">
-            Logo
-          </Box>
-          <Flex
-            className="nav"
-            backgroundColor={theme.color.brand.bgBlue}
-            textStyle="body"
-          >
-            <Show above="lg">
-              <Link>{"mercury".toUpperCase()}</Link>
-              <Link>{"venus".toUpperCase()}</Link>
-              <Link>{"earth".toUpperCase()}</Link>
-              <Link>{"mars".toUpperCase()}</Link>
-              <Link>{"jupiter".toUpperCase()}</Link>
-              <Link>{"saturn".toUpperCase()}</Link>
-              <Link>{"uranus".toUpperCase()}</Link>
-              <Link>{"neptune".toUpperCase()}</Link>
-            </Show>
-            <Show below="lg">
-              <Button onClick={onToggle}>Click Me</Button>
-              <Collapse in={isOpen} animateOpacity>
+        <Show below="sm">
+          <Flex className="header">
+            <Logo />
+            <Flex
+              className="nav"
+              backgroundColor={theme.color.brand.bgBlue}
+              textStyle="body"
+            >
+              <Show above="lg">
                 <Link>{"mercury".toUpperCase()}</Link>
                 <Link>{"venus".toUpperCase()}</Link>
                 <Link>{"earth".toUpperCase()}</Link>
@@ -50,51 +38,64 @@ function App() {
                 <Link>{"saturn".toUpperCase()}</Link>
                 <Link>{"uranus".toUpperCase()}</Link>
                 <Link>{"neptune".toUpperCase()}</Link>
-              </Collapse>
-            </Show>
+              </Show>
+              <Show below="lg">
+                <Button onClick={onToggle}>Click Me</Button>
+                <Collapse in={isOpen} animateOpacity>
+                  <Link>{"mercury".toUpperCase()}</Link>
+                  <Link>{"venus".toUpperCase()}</Link>
+                  <Link>{"earth".toUpperCase()}</Link>
+                  <Link>{"mars".toUpperCase()}</Link>
+                  <Link>{"jupiter".toUpperCase()}</Link>
+                  <Link>{"saturn".toUpperCase()}</Link>
+                  <Link>{"uranus".toUpperCase()}</Link>
+                  <Link>{"neptune".toUpperCase()}</Link>
+                </Collapse>
+              </Show>
+            </Flex>
           </Flex>
-        </Flex>
-        <Box className="info-buttons">
-          <Button textStyle="h3" className="info-buttons--button">
-            {"overview".toUpperCase()}
-          </Button>
-          <Button textStyle="h3" className="info-buttons--button">
-            {"structure".toUpperCase()}
-          </Button>
-          <Button textStyle="h3" className="info-buttons--button">
-            {"surface".toUpperCase()}
-          </Button>
-        </Box>
-        <Image className="planet-img" src="./src/assets/planet-mercury.svg" />
-        <Box>
-          <Box textStyle="h1">{"mercury".toUpperCase()}</Box>
-          <Text textStyle="body" marginBottom="3rem">
-            Mercury is the smallest planet in the Solar System and the closest
-            to the Sun. Its orbit around the Sun takes 87.97 Earth days, the
-            shortest of all the Sun's planets. Mercury is one of four
-            terrestrial planets in the Solar System, and is a rocky body like
-            Earth.
-          </Text>
-          <Link textStyle="body">Source</Link>
-        </Box>
-        <Box className="planet-facts">
-          <Flex className="planet-facts--fact">
-            <Text textStyle="body">{"rotation time".toUpperCase()}</Text>
-            <Text textStyle="h2">58.6 DAYS</Text>
-          </Flex>
-          <Flex className="planet-facts--fact">
-            <Text textStyle="body">{"rotation time".toUpperCase()}</Text>
-            <Text textStyle="h2">58.6 DAYS</Text>
-          </Flex>
-          <Flex className="planet-facts--fact">
-            <Text textStyle="body">{"rotation time".toUpperCase()}</Text>
-            <Text textStyle="h2">58.6 DAYS</Text>
-          </Flex>
-          <Flex className="planet-facts--fact">
-            <Text textStyle="body">{"rotation time".toUpperCase()}</Text>
-            <Text textStyle="h2">58.6 DAYS</Text>
-          </Flex>
-        </Box>
+          <Box className="info-buttons">
+            <Button textStyle="h3" className="info-buttons--button">
+              {"overview".toUpperCase()}
+            </Button>
+            <Button textStyle="h3" className="info-buttons--button">
+              {"structure".toUpperCase()}
+            </Button>
+            <Button textStyle="h3" className="info-buttons--button">
+              {"surface".toUpperCase()}
+            </Button>
+          </Box>
+          <Image className="planet-img" src="./src/assets/planet-mercury.svg" />
+          <Box>
+            <Box textStyle="h1">{"mercury".toUpperCase()}</Box>
+            <Text textStyle="body" marginBottom="3rem">
+              Mercury is the smallest planet in the Solar System and the closest
+              to the Sun. Its orbit around the Sun takes 87.97 Earth days, the
+              shortest of all the Sun's planets. Mercury is one of four
+              terrestrial planets in the Solar System, and is a rocky body like
+              Earth.
+            </Text>
+            <Link textStyle="body">Source</Link>
+          </Box>
+          <Box className="planet-facts">
+            <Flex className="planet-facts--fact">
+              <Text textStyle="body">{"rotation time".toUpperCase()}</Text>
+              <Text textStyle="h2">58.6 DAYS</Text>
+            </Flex>
+            <Flex className="planet-facts--fact">
+              <Text textStyle="body">{"rotation time".toUpperCase()}</Text>
+              <Text textStyle="h2">58.6 DAYS</Text>
+            </Flex>
+            <Flex className="planet-facts--fact">
+              <Text textStyle="body">{"rotation time".toUpperCase()}</Text>
+              <Text textStyle="h2">58.6 DAYS</Text>
+            </Flex>
+            <Flex className="planet-facts--fact">
+              <Text textStyle="body">{"rotation time".toUpperCase()}</Text>
+              <Text textStyle="h2">58.6 DAYS</Text>
+            </Flex>
+          </Box>
+        </Show>
       </VStack>
     </>
   );
---
+++ src/Components/Logo.tsx
@@ -0,0 +1,14 @@
+import { Box, Text } from "@chakra-ui/react";
+import "../index.css";
+
+const Logo = () => {
+  return (
+    <>
+      <Box className="logo">
+        <Text className="logo--text">THE PLANETS</Text>
+      </Box>
+    </>
+  );
+};
+
+export default Logo;
